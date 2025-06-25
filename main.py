6d50da6a1903846cfe9ba3d1e408eac5af442b0e
MR-Khan0099 <mohammedrizwan0909@gmail.com>
2025-06-25 21:59:29 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.DataScienceProject import logger
-from src.DataScienceProject.pipeline.data_ingestion_pipeline import DataIngestion_TrainingPipeline  
+from src.DataScienceProject.pipeline.data_ingestion_pipeline import DataIngestion_TrainingPipeline
+from src.DataScienceProject.pipeline.data_validation_pipeline import DataValidation_TrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -14,7 +16,15 @@ except Exception as e:
 
 
 
-
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation = DataValidation_TrainingPipeline()
+    data_validation.initiate_data_validation()
+    logger.info(f"Stage {STAGE_NAME} completed!")
+except Exception as e:
+    logger.error(f"Error occurred in stage {STAGE_NAME}: {e}")
+    raise e 
 
 
 
--- src/DataScienceProject/entity/config_entity.py
+++ src/DataScienceProject/entity/config_entity.py
@@ -7,3 +7,11 @@ class DataIngestionConfig:
     source_URL: str = str
     local_data_file: Path = Path
     unzip_dir: Path = Path
+
+
+@dataclass
+class DataValidationConfig:
+    root_dir: Path = Path
+    unzip_data_dir: Path = Path
+    STATUS_FILE:str = str
+    all_schema:dict = dict
